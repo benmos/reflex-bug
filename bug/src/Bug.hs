2eb085b5fde72033be4ed79b1e9327b164275fa1
Ben Moseley <ben@moseley.name>
2015-10-14 16:51:40 +0100

    Simplify to single instance of 'simpleList'

--- bug/src/Bug.hs
+++ bug/src/Bug.hs
@@ -93,12 +93,10 @@ main = mainWidgetWithHead (el "style" $ do
   el "p" $ text "If you switch between the feeds quickly enough you will see that the DOM 'div' nodes for one of the feeds sometimes get 'orphaned' and left in the DOM - this leads to the items of two feeds being shown simultaneously which should never happen."
   rec state      <- foldDyn processEvent initialState $ leftmost [efetched, fmap FeedSelected esel]
       ditems     <- mapDyn (feedRBItems . view selFeed) state -- Items of selected feed
-      dbfms      <- mapDyn (view asFeeds)               state
-      dsel       <- mapDyn (view asSelectedFeed)        state
 
       -- Render the UI
-      esel       <- feedList dsel dbfms -- Returns Event of RBFeed clicks
-      void        $ itemList ditems
+      esel       <- feedList
+      void        $ simpleList ditems (el "div" . dynText <=< mapDyn (T.unpack . itemTitle))
 
       efetched   <- fetchFeed esel -- Load the selected RSS Feed
 
@@ -111,60 +109,36 @@ main = mainWidgetWithHead (el "style" $ do
 fetchFeed :: forall t m . MonadWidget t m => Event t RBFeed -> m (Event t AppEvent)
 fetchFeed evt = do
       let req f = xhrRequest "GET" (show $ f^.feedURL) def
-      fmap FeedFetched . traceEventWith (\s -> "[AJAX_RETURNED] " <> show (length s)) <$>
+      fmap FeedFetched <$>
         performAJAX req (maybe "nope" T.unpack . _xhrResponse_body) evt
 
 -- | Parse the Feed
 feedRBItems :: Maybe (T.Text,Feed) -> [RBFeedItem]
-feedRBItems (Just (s, RSSFeed rss)) = [RBFeedItem (maybe "NO_TITLE" ((s <>) . (" : " <>) . T.pack) $ rssItemTitle ri)
-                                                 (parseURI =<< rssItemLink ri) |
-                                      ri <- rssItems $ rssChannel rss]
+feedRBItems (Just (s, RSSFeed rss)) = [RBFeedItem (maybe "NO_TITLE" prepTitle $ rssItemTitle ri)
+                                                  (parseURI =<< rssItemLink ri) |
+                                       let prepTitle = ((s <>) . (" : " <>) . T.pack),
+                                       ri <- rssItems $ rssChannel rss]
 feedRBItems _                       = []
 
 ------------------------------------------------------------------------
 
 -- | Render a list of Feeds.
 --   Returns an Event of clicked-on RBFeeds
-feedList :: forall t m . MonadWidget t m => Dynamic t RBFeed -> Dynamic t (M.Map RBFeed RBFeedState) -> m (Event t RBFeed)
-feedList dsel ds = el "ul" $ selectViewListWithKey_ dsel ds feed
+feedList :: forall t m . MonadWidget t m => m (Event t RBFeed)
+feedList = el "ul" $ do
+  let [f1,f2,f3] = rawFeeds
+  (elt1,_) <- el' "li" $ text "Feed 1"
+  (elt2,_) <- el' "li" $ text "Feed 2"
+  (elt3,_) <- el' "li" $ text "Feed 3"
 
-
--- | Render an individual Feed
---   Returns an Event of when the Feed is clicked-on
-feed :: forall t m . MonadWidget t m => RBFeed -> Dynamic t RBFeedState -> Dynamic t Bool -> m (Event t RBFeed)
-feed f dfeedst dsel = mdo
-  let styleItem :: RBFeedState -> M.Map String String
-      styleItem = ("class" =:) . rbfeedstate "notloaded" "loading" (maybe "failed" (const "loaded"))
-
-      numItems :: RBFeedState -> String
-      numItems = rbfeedstate "[?] " "[...] " (("["<>) . (<>"] ") . show . length . feedRBItems . fmap ("",))
-
-  dfeedcnt  <- mapDyn numItems  dfeedst
-  datts     <- mapDyn styleItem dfeedst
-  dattssel  <- selClassIf datts dsel
-
-  (elt,_)   <- elDynAttr' "li" dattssel (do
-                                      el "span" $ dynText dfeedcnt
-                                      text $ T.unpack $ f^.feedName
-                                     )
-  return $ traceEvent "[FEED_CHOSEN]:"             $ fmap (const f) $
-           traceEventWith (const "[FEED_CLICKED]") $ domEvent Click elt
-
-
-
-itemList :: MonadWidget t m => Dynamic t [RBFeedItem] -> m ()
-itemList items = void $ simpleList items item
-
-item :: forall t m . MonadWidget t m => Dynamic t RBFeedItem -> m ()
-item disel = do
-  dtitle <- mapDyn (T.unpack . itemTitle) disel
-  el "div" $ dynText dtitle
+  return $ leftmost [
+              f1 <$ domEvent Click elt1,
+              f2 <$ domEvent Click elt2,
+              f3 <$ domEvent Click elt3
+             ]
+  
 
 ------------------------------------------------------------------------
-
-
-staticUri :: String -> URI
-staticUri s = fromMaybe (error $ "Bad static URI: " <> s) $ parseURI s
 
 rawFeeds :: [RBFeed]
 rawFeeds = [
@@ -172,6 +146,8 @@ rawFeeds = [
   RBFeed "Feed 2" (staticUri "https://www.middevonnewscentre.info/feed/"),
   RBFeed "Feed 3" (staticUri "https://www.devoncommunities.org.uk/handlers/rss.ashx?feed=1&amp;IDBlogGroup=e5ec3ceb-8a9c-4a03-ab38-6d0c97b5c49e")
   ]
+  where
+    staticUri s = fromMaybe (error $ "Bad static URI: " <> s) $ parseURI s
 
 
 -- | This is the foundational primitive for the XHR API because it gives you
@@ -189,10 +165,4 @@ performAJAX mkRequest parseResponse req =
       _ <- newXMLHttpRequest (mkRequest a) $ \response ->
              liftIO $ cb (a, parseResponse response)
       return ()
-
--- | Add a 'sel' class to (dynamically) indicate whether something is selected or not
-selClassIf :: MonadWidget t m => Dynamic t (M.Map String String) -> Dynamic t Bool -> m (Dynamic t (M.Map String String))
-selClassIf dattr dsel = combineDyn (\attr sel -> if sel then ("class" =: "sel") .<>. attr else attr) dattr dsel
-  where
-    (.<>.) = M.unionWith (\v1 v2 -> v1 <> "  " <> v2) -- Combine eg multiple "class" attr vlas
 
