3b69b39dc69fd5e71453d990818da68cd78c418b
Ben Moseley <ben@moseley.name>
2015-10-15 16:22:38 +0100

    Use try-reflex by importing rather than modifying
    Delete unneeded files

--- reflex-dom/git.nix
+++
@@ -1,5 +0,0 @@
-{
-  url = git://github.com/ryantrinkle/reflex-dom.git;
-  rev = "a6006d6727f91185838fb84a2acc443692d58d5e";
-  sha256 = "3dd1d70a7cc16295056cce44aa3677e0bcef1c1fa49306633bf5326090d5e6df";
-}
--- reflex/git.nix
+++
@@ -1,5 +0,0 @@
-{
-  url = git://github.com/ryantrinkle/reflex;
-  rev = "784f9f79396ae713a03fc4d81f8a67b75d6ade1c";
-  sha256 = "7e8c44c8ac827d7db1744f6a0f5b55237cd4e0fa883a1e2553c1f257b0684c2c";
-}
